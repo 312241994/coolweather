21bd12b40e8787f44d011fe0eb39df1c15583f0d
laowang <312241994@qq.com>
2017-02-18 21:01:19 +0800

    add json mode

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by changhe on 2017/2/18.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
